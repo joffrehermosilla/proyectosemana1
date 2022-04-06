9ae756b86189e8bb32a96f2d05c58eea1f3bf852
joffre <joffre.hermosilla@gmail.com>
2022-04-05 23:20:56 -0500

    Primer commit creacion de microservicio eureka server

---
+++ proyectosemana1/microservicios-eureka/src/main/java/bootcamp/microservicios/semanauno/eureka/MicroserviciosEurekaApplication.java
@@ -0,0 +1,16 @@
+package bootcamp.microservicios.semanauno.eureka;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviciosEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosEurekaApplication.class, args);
+	}
+
+}
---
+++ proyectosemana1/microservicios-eureka/src/test/java/bootcamp/microservicios/semanauno/eureka/MicroserviciosEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.microservicios.semanauno.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
