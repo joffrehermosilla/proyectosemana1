f1a721889e68e04103a930a405bdaa834e1a681f
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-07 10:16:09 -0500

    Sexto commit creacion del segundo microservicios client con conexion a mongo segun las pautas del proyecto

---
+++ microservicios-cliente/src/main/java/bootcamp/microservicios/semanauno/client/MicroserviciosClienteApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.microservicios.semanauno.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosClienteApplication.class, args);
+	}
+
+}
---
+++ microservicios-cliente/src/test/java/bootcamp/microservicios/semanauno/client/MicroserviciosClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.microservicios.semanauno.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
