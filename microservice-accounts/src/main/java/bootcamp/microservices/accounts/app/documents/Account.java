c32858e51c1b2f91cdd16e4c9f12b90471b3d1f2
Lulio Arturo Herrera Mestanza <lherrerm@emeal.nttdata.com>
2022-04-07 16:36:39 -0500

    Actualizacion microservicio cuentas

--- microservice-accounts/src/main/java/bootcamp/microservices/accounts/app/documents/Account.java
+++ microservice-accounts/src/main/java/bootcamp/microservices/accounts/app/documents/Account.java
@@ -1,10 +1,30 @@
 package bootcamp.microservices.accounts.app.documents;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Account {
 	
 	private String id;
 	
 	private String accountNumber;
+	
+	private int accountType;
+	
+	private String idClientCompany;
+	
+	private Double amount;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy")	
+	private Date createDate;
+	
+	private String createUser;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy")	
+	private Date modifyDate;
+	
+	private String modifyUser;
 	
 
 }
